7644358f00620cc148b164d2e1fc99a270c52334
Ab <4b5huja@gmail.com>
2023-04-15 04:52:44 +0200

    comment

--- apps/blog/src/components/Click.tsx
+++ apps/blog/src/components/Click.tsx
@@ -1,5 +1,10 @@
 import { Button } from "ui";
 
+/*
+    This is here because a React component imported into an Astro page can't
+    directly be passed a function as a prop, since functions aren't serializable.
+*/
+
 export default function Click() {
   return <Button onClick={() => console.log("hello")} />;
 }
